f0cce49743dfb2b7367a8a3b400fd816bd5416e9
jaygraygray <jgray@mediasilo.com>
2018-12-19 13:55:54 -0500

    Deploy changes

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3001;
+const port = process.env.port || 3001;
+
+app.use(express.static(__dirname + '/dist'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(
-  process.env.PORT || port,
+  port,
   () => console.log(`Wordsmith listening on port ${port}`)
 );
